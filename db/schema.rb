922e2fc338c60726aee9658f2e81dd0cddb1f6aa
Adam Pogwizd <adampog77@gmail.com>
2019-11-16 11:47:53 -0600

    added constraints to user table

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,12 @@ ActiveRecord::Schema.define(version: 2019_11_10_205152) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

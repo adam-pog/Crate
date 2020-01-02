373b0cd21f908faf83fe92396b660e93bab18f31
Adam Pogwizd <adampog77@gmail.com>
2020-01-01 19:59:43 -0600

    added styling to new cataegory page

--- crate-client/src/budget/NewBudgetCategory.js
+++ crate-client/src/budget/NewBudgetCategory.js
@@ -2,6 +2,32 @@ import React from 'react';
 import './NewBudgetCategory.css';
 import { Fetch } from '../FetchHelper.js'
 import history from '../config/history'
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  login: {
+    marginBottom: theme.spacing(20),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(2, 0),
+  },
+  input: {
+    margin: theme.spacing(2, 0)
+  },
+  avatar: {
+    backgroundColor: theme.palette.secondary.main,
+  }
+});
 
 class NewBudgetCategory extends React.Component {
   state = {
@@ -33,29 +59,57 @@ class NewBudgetCategory extends React.Component {
   }
 
   render() {
-    return(
-      <div className="App">
-        <header className="App-header">
-          <p>
-            New budget Category
-          </p>
-        </header>
-        <form onSubmit={(e) => this.handleSubmit(e)}>
-          <input
-            name="label"
-            placeholder="Label"
-            onChange={(e) => this.handleFieldChange(e)}
-          />
-          <input
-            name="monthly_amount"
-            placeholder="Monthly Amount"
-            onChange={(e) => this.handleFieldChange(e)}
-          />
-          <input type="submit" value="Submit"/>
+    const { classes } = this.props;
+
+    return (
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        className={classes.login}
+      >
+        <Typography component='h1' variant='h2' color='textPrimary'>
+          New Budget
+        </Typography>
+        <form onSubmit={(e) => this.handleSubmit(e)} className={classes.form}>
+          <Grid container className={classes.input}>
+            <TextField
+              label="Label"
+              variant="outlined"
+              name="label"
+              autoComplete="off"
+              onChange={(e) => this.handleFieldChange(e)}
+            />
+          </Grid>
+          <Grid container className={classes.input}>
+            <TextField
+              variant="outlined"
+              label="Monthly Amount"
+              name="monthly_amount"
+              autoComplete="off"
+              onChange={(e) => this.handleFieldChange(e)}
+            />
+          </Grid>
+          <Grid
+            container
+            justify="center"
+            alignItems="center"
+            className={classes.submit}
+          >
+            <Button
+              fullWidth
+              type="submit"
+              variant="contained"
+              color="primary"
+            >
+              Add Budget
+            </Button>
+          </Grid>
         </form>
-      </div>
+      </Grid>
     )
   }
 }
 
+NewBudgetCategory = withStyles(styles)(NewBudgetCategory);
 export default NewBudgetCategory;

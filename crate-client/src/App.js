d0fd00c0e9a41c97b85cec284961f808e2de4c7e
Adam Pogwizd <adampog77@gmail.com>
2021-06-04 17:05:58 -0600

    move terminal logic outside of login component

--- crate-client/src/App.js
+++ crate-client/src/App.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import './App.scss';
-import { Route, Router, Switch, Redirect } from 'react-router-dom'
 import Login from './Login.js'
 import { Fetch } from './FetchHelper.js'
 import { connect } from "react-redux";
-import history from './config/history';
-import { setAuthenticated } from "./actions/index";
+import { setAuthenticated, addCommandHistory } from "./actions/index";
+import { animateScroll } from 'react-scroll'
+import TextareaAutosize from 'react-textarea-autosize';
 
 const mapStateToProps = state => {
-  return { authenticated: state.authenticated, name: state.name };
+  return {
+    authenticated: state.authenticated,
+    name: state.name,
+    commandHistory: state.commandHistory
+  };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     setAuthenticated: authenticated => (
       dispatch(setAuthenticated(authenticated))
-    )
+    ),
+    addCommandHistory: value => {
+      dispatch(addCommandHistory(value))
+    }
   };
 }
 
-const PrivateRoute = ({ component: Component, authenticated, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    authenticated
-      ? <Component {...props} />
-      : <Redirect to='/' />
-  )} />
-)
-
 class App extends React.Component {
+  state = {
+    text: '',
+    command: ''
+  }
+
   logout() {
     Fetch('logout', 'post')
     .then(([status, _response]) => {
@@ -40,22 +44,73 @@ class App extends React.Component {
     })
   }
 
+  renderLogin() {
+    return <Login />
+  }
+
+  renderTerminal() {
+    return <div className='terminal' id='terminal'>
+      <div>
+        {this.renderCommandHistory()}
+      </div>
+      <div className='promptContainer'>
+        <h3 className='prompt'>></h3>
+        <TextareaAutosize
+          spellCheck={false}
+          className='shell'
+          autoFocus
+          value={this.state.text}
+          onChange={(e) => this.onChange(e.target.value)}
+          onKeyDown={(e) => this.onKeyDown(e)}
+        >
+        </TextareaAutosize>
+      </div>
+    </div>
+  }
+
+  renderSwitch() {
+    switch(this.state.command) {
+      case 'login':
+        return this.renderLogin();
+      default:
+        return this.renderTerminal()
+    }
+  }
+
+  onKeyDown(e) {
+    if (e.keyCode === 9) {
+      e.preventDefault();
+      // setText(text + 'something')
+      console.log('implement autocomplete')
+    } else if (e.keyCode === 13) {
+      e.preventDefault();
+      this.props.addCommandHistory([`>${this.state.text}`, `${this.state.text}: command not found`]);
+      this.setState({ text: '', command: this.state.text })
+    }
+  }
+
+  onChange(text) {
+    this.setState({ text })
+  }
+
+  renderCommandHistory() {
+    return this.props.commandHistory.map((command, i) => (
+      <p key={i} className='command'>{command}</p>
+    ))
+  }
+
+  scrollToBottom() {
+    animateScroll.scrollToBottom({
+      containerId: 'terminal',
+      duration: 0,
+      offset: 100
+    })
+  }
+
   render() {
     return (
       <div className='App'>
-        <Router history={ history }>
-          <Switch>
-            <Route exact path='/'>
-              { !this.props.authenticated &&
-                <Login />
-              }
-            </Route>
-
-            <Route>
-              <Redirect to='/' />
-            </Route>
-          </Switch>
-        </Router>
+        {this.renderSwitch()}
       </div>
     )
   }
--- crate-client/src/Login.js
+++ crate-client/src/Login.js
@@ -1,11 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Fetch } from './FetchHelper.js'
-import { setAuthenticated, addCommandHistory } from "./actions/index";
+import { setAuthenticated } from "./actions/index";
 import { connect } from "react-redux";
-import history from './config/history'
-import TextareaAutosize from 'react-textarea-autosize';
-import { animateScroll } from 'react-scroll'
 
 
 
@@ -13,10 +10,7 @@ const mapDispatchToProps = dispatch => {
   return {
     setAuthenticated: authenticated => (
       dispatch(setAuthenticated(authenticated))
-    ),
-    addCommandHistory: value => {
-      dispatch(addCommandHistory(value))
-    }
+    )
   };
 }
 
@@ -32,7 +26,6 @@ const handleSubmit = (e, email, password, set) => {
   .then(([status, response]) => {
     if(status === 200) {
       set({authenticated: true, name: response.name})
-      history.push('/')
     } else {
       console.log('uh oh')
     }
@@ -40,67 +33,14 @@ const handleSubmit = (e, email, password, set) => {
 }
 
 function Login({
-  classes,
   setAuthenticated,
-  addCommandHistory,
-  commandHistory
 }) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [text, setText] = useState('')
-
-  useEffect(() => {
-    scrollToBottom();
-  })
-
-  const onKeyDown = (e) => {
-    if (e.keyCode === 9) {
-      e.preventDefault();
-      // setText(text + 'something')
-      console.log('implement autocomplete')
-    } else if (e.keyCode === 13) {
-      e.preventDefault();
-      addCommandHistory([`>${text}`, `${text}: command not found`]);
-      setText('');
-    }
-  }
-
-  const onChange = (value) => {
-    setText(value)
-  }
-
-  const renderCommandHistory = () => (
-    commandHistory.map((command, i) => (
-      <p key={i} className='command'>{command}</p>
-    ))
-  )
-
-  const scrollToBottom = () => {
-    animateScroll.scrollToBottom({
-      containerId: 'terminal',
-      duration: 0,
-      offset: 100
-    })
-  }
-
+  console.log('watwatat')
   return (
-    <div className='terminal' id='terminal'>
-      <div>
-        {renderCommandHistory()}
-      </div>
-      <div className='promptContainer'>
-        <h3 className='prompt'>></h3>
-        <TextareaAutosize
-          spellCheck={false}
-          className='shell'
-          autoFocus
-          value={text}
-          onChange={(e) => onChange(e.target.value)}
-          onKeyDown={(e) => onKeyDown(e)}
-        >
-        </TextareaAutosize>
-      </div>
-    </div>
+    <p styles={{color: 'white'}}>Login stuff hewre</p>
+    // render stuff here?
   )
 }
 
